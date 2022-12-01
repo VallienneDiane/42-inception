497e7af273beaeb1fb81bff25404dad7850f8089
dvallien <diane.vallienne@outlook.com>
2022-12-01 17:57:42 +0100

    miss makefile

--- srcs/requirements/wordpress/tools/entrypoint.sh
+++ srcs/requirements/wordpress/tools/entrypoint.sh
@@ -5,11 +5,23 @@ if test -f "$FILE"; then
 	rm $FILE
 fi
 
-# CONFIGURE FILE WP-CONFIG.PHP
-wp core config  --allow-root --dbname=$MYSQL_DATABASE --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --dbhost=$MYSQL_HOST --dbprefix=wp_
+# if ! wp core is-installed --allow-root; then	
+	# Generates a wp-config.php file. && CONFIGURE FILE WP-CONFIG.PHP  	
+	wp config create --path="/var/www/html/wordpress" --allow-root --dbname=$DB_DATABASE --dbuser=$DB_ADMIN --dbpass=$DB_ADMIN_PASSWORD --dbhost=$DB_HOST --dbprefix=wp_
 
-# CREATE THE WP TABLES IN THE DATABASE
-wp core install --allow-root --url=$DOMAIN_NAME --title="$WP_SITE_TITLE" --admin_user=$WP_ADMIN --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_EMAIL
+	# Runs the standard WordPress installation process - Create the wp tables in database
+	wp core install --path="/var/www/html/wordpress" --allow-root --url=$DOMAIN_NAME --title="$DB_SITE_TITLE" --admin_user=$DB_ADMIN --admin_password=$DB_ADMIN_PASSWORD --admin_email=$DB_ADMIN_EMAIL
+	
+	# Create database, user and admin 
+	# wp db create --allow-root
+	wp user create --allow-root $DB_USER $DB_USER_EMAIL --user_pass=$DB_USER_PASSWORD
+	
+	# &&  mysql -u root -e    "CREATE DATABASE IF NOT EXISTS $DB_DATABASE; \
+    #                     ALTER USER 'root'@'localhost' IDENTIFIED BY '$DB_ADMIN_PASSWORD'; \
+    #                     GRANT ALL PRIVILEGES ON $DB_DATABASE.* TO '$DB_ADMIN'@'%' IDENTIFIED BY '$DB_ADMIN_PASSWORD'; \
+    #                     FLUSH PRIVILEGES;"
+	# wp theme install neve --activate --allow-root
+# fi
 
 mkdir -p /run/php/
 php-fpm7.3 -F 
