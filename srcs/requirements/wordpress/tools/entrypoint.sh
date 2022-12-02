99899d22647148f4030c57d507a300930386cbf1
dvallien <diane.vallienne@outlook.com>
2022-12-02 16:09:56 +0100

    almost done

--- srcs/requirements/wordpress/tools/entrypoint.sh
+++ srcs/requirements/wordpress/tools/entrypoint.sh
@@ -1,27 +1,20 @@
 cd /var/www/html/wordpress
 
-FILE=/var/www/html/wordpress/wp-config.php
-if test -f "$FILE"; then
-	rm $FILE
-fi
+# DOWNLOAD WORDPRESS FILES
+wp core download  --path="/var/www/html/wordpress" --allow-root
 
-# if ! wp core is-installed --allow-root; then	
-	# Generates a wp-config.php file. && CONFIGURE FILE WP-CONFIG.PHP  	
-	wp config create --path="/var/www/html/wordpress" --allow-root --dbname=$DB_DATABASE --dbuser=$DB_ADMIN --dbpass=$DB_ADMIN_PASSWORD --dbhost=$DB_HOST --dbprefix=wp_
+# GENERATE AND CONFIGURE THE WP-CONFIG.PHP FILE
+wp config create --path="/var/www/html/wordpress" --allow-root --dbname=$DB_DATABASE --dbuser=$DB_USER --dbpass=$DB_USER_PASSWORD --dbhost=$DB_HOST --dbprefix=wp_
 
-	# Runs the standard WordPress installation process - Create the wp tables in database
-	wp core install --path="/var/www/html/wordpress" --allow-root --url=$DOMAIN_NAME --title="$DB_SITE_TITLE" --admin_user=$DB_ADMIN --admin_password=$DB_ADMIN_PASSWORD --admin_email=$DB_ADMIN_EMAIL
-	
-	# Create database, user and admin 
-	# wp db create --allow-root
-	wp user create --allow-root $DB_USER $DB_USER_EMAIL --user_pass=$DB_USER_PASSWORD
-	
-	# &&  mysql -u root -e    "CREATE DATABASE IF NOT EXISTS $DB_DATABASE; \
-    #                     ALTER USER 'root'@'localhost' IDENTIFIED BY '$DB_ADMIN_PASSWORD'; \
-    #                     GRANT ALL PRIVILEGES ON $DB_DATABASE.* TO '$DB_ADMIN'@'%' IDENTIFIED BY '$DB_ADMIN_PASSWORD'; \
-    #                     FLUSH PRIVILEGES;"
-	# wp theme install neve --activate --allow-root
-# fi
+# RUNS THE STANDARD WORDPRESS INSTALLATION PROCESS - CREATE THE WORDPRESS TABLES IN DB 
+wp core install --path="/var/www/html/wordpress" --allow-root --url=$DOMAIN_NAME --title="$WP_SITE_TITLE" --admin_user=$WP_ADMIN --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_EMAIL
 
+# CHOOSE THEME
+# wp theme install neve --activate --allow-root
+
+# GIVE PERMISSIONS TO GROUP AND USER WWW-DATA TO UPLOADS | -R EXECUTE ROOT
+chown www-data:www-data /var/www/html/wordpress/wp-content/uploads -R
+
+# LAUNCH PHP-FPM
 mkdir -p /run/php/
 php-fpm7.3 -F 
